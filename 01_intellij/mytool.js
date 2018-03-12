d4d9e52b62ba01ff8f845c1ed613b32608c22d85
yinm <yinm.cminor9@gmail.com>
2018-03-12 22:32:44 +0900

    Check option + Enter

--- 01_intellij/mytool.js
+++ 01_intellij/mytool.js
@@ -1,9 +1,9 @@
 function something(x, y) {
-    if (x=="0"){
-      return {
-        x: 0
-      }
-    }else
+    if (x == '0'){
+        return {
+            x: 0
+        };
+    } else
     {
         z = x + y;
         return z ;
