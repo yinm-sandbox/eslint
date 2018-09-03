fca52017af93a830bffb200afd183183f32b46d5
yinm <yinm.cminor9@gmail.com>
2018-09-03 21:04:56 +0900

    Add webpack.config.js

---
+++ eslint-vue-typescript-sample/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  mode: 'development',
+
+  entry: './src/main.ts',
+
+  devtool: '#eval-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json', '.ts']
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
